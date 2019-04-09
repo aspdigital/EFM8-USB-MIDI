c94835670d88ca6c9743c3f09de67a5c4b891400
andy <andy@ae0baf16-264e-11e7-85bd-8384b197c6a9>
2019-04-09 00:11:29 +0000

    Add a lot of stuff from the STK we can use to expand this demo project.

--- src/Interrupts.c
+++ src/Interrupts.c
@@ -32,3 +32,60 @@
 
 //}
 
+//-----------------------------------------------------------------------------
+// TIMER2_ISR
+//-----------------------------------------------------------------------------
+//
+// TIMER2 ISR Content goes here. Remember to clear flag bits:
+// TMR2CN0::TF2H (Timer # High Byte Overflow Flag)
+// TMR2CN0::TF2L (Timer # Low Byte Overflow Flag)
+//
+//-----------------------------------------------------------------------------
+SI_INTERRUPT (TIMER2_ISR, TIMER2_IRQn)
+{
+
+}
+
+//-----------------------------------------------------------------------------
+// SPI0_ISR
+//-----------------------------------------------------------------------------
+//
+// SPI0 ISR Content goes here. Remember to clear flag bits:
+// SPI0CN0::MODF (Mode Fault Flag)
+// SPI0CN0::RXOVRN (Receive Overrun Flag)
+// SPI0CN0::SPIF (SPI# Interrupt Flag)
+// SPI0CN0::WCOL (Write Collision Flag)
+//
+//-----------------------------------------------------------------------------
+SI_INTERRUPT (SPI0_ISR, SPI0_IRQn)
+{
+
+}
+
+//-----------------------------------------------------------------------------
+// ADC0EOC_ISR
+//-----------------------------------------------------------------------------
+//
+// ADC0EOC ISR Content goes here. Remember to clear flag bits:
+// ADC0CN0::ADINT (Conversion Complete Interrupt Flag)
+//
+//-----------------------------------------------------------------------------
+SI_INTERRUPT (ADC0EOC_ISR, ADC0EOC_IRQn)
+{
+
+}
+
+//-----------------------------------------------------------------------------
+// TIMER3_ISR
+//-----------------------------------------------------------------------------
+//
+// TIMER3 ISR Content goes here. Remember to clear flag bits:
+// TMR3CN0::TF3H (Timer # High Byte Overflow Flag)
+// TMR3CN0::TF3L (Timer # Low Byte Overflow Flag)
+//
+//-----------------------------------------------------------------------------
+SI_INTERRUPT (TIMER3_ISR, TIMER3_IRQn)
+{
+
+}
+
