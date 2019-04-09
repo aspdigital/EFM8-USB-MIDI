83448d775028dd3abfe858460a42075432fa5ab6
andy <andy@ae0baf16-264e-11e7-85bd-8384b197c6a9>
2019-04-09 17:27:54 +0000

    We don't use the PCA. But we need the headers for these other things.

--- inc/config/efm8_config.h
+++ inc/config/efm8_config.h
@@ -7,4 +7,9 @@
 #ifndef __EFM8_CONFIG_H__
 #define __EFM8_CONFIG_H__
 
+#define EFM8PDL_SPI0_USE                  1
+#define EFM8PDL_SPI0_USE_BUFFER           1
+#define EFM8PDL_SPI0_USE_FIFO             0
+#define EFM8PDL_SPI0_TX_SEGTYPE           SI_SEG_IDATA
+
 #endif // __EFM8_CONFIG_H__
