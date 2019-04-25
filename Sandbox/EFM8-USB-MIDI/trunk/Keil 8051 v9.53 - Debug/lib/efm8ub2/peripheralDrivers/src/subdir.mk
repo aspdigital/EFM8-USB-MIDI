40635c587cc92e515a793bcf09befc9bdfa81c90
andy <andy@ae0baf16-264e-11e7-85bd-8384b197c6a9>
2019-04-25 04:30:38 +0000

    For USB OUT on EP 1, we “prime the pump” by calling USBD_Read() with a buffer, then in the callback we copy the contents of that buffer to the message packet we parse in main. Then we call USBD_Read() again, using that endpoint buffer.

--- Sandbox/EFM8-USB-MIDI/trunk/Keil 8051 v9.53 - Debug/lib/efm8ub2/peripheralDrivers/src/subdir.mk
+++ Sandbox/EFM8-USB-MIDI/trunk/Keil 8051 v9.53 - Debug/lib/efm8ub2/peripheralDrivers/src/subdir.mk
@@ -6,11 +6,13 @@
 C_SRCS += \
 ../lib/efm8ub2/peripheralDrivers/src/adc_0.c \
 ../lib/efm8ub2/peripheralDrivers/src/spi_0.c \
+../lib/efm8ub2/peripheralDrivers/src/uart_1.c \
 ../lib/efm8ub2/peripheralDrivers/src/usb_0.c 
 
 OBJS += \
 ./lib/efm8ub2/peripheralDrivers/src/adc_0.OBJ \
 ./lib/efm8ub2/peripheralDrivers/src/spi_0.OBJ \
+./lib/efm8ub2/peripheralDrivers/src/uart_1.OBJ \
 ./lib/efm8ub2/peripheralDrivers/src/usb_0.OBJ 
 
 
@@ -18,14 +20,16 @@ OBJS += \
 lib/efm8ub2/peripheralDrivers/src/%.OBJ: ../lib/efm8ub2/peripheralDrivers/src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: Keil 8051 Compiler'
-	C51 "@$(patsubst %.OBJ,%.__i,$@)" || $(RC)
+	wine "/Applications/Simplicity Studio.app/Contents/Eclipse/developer/toolchains/keil_8051/9.53/BIN/C51" "@$(patsubst %.OBJ,%.__i,$@)" || $(RC)
 	@echo 'Finished building: $<'
 	@echo ' '
 
-lib/efm8ub2/peripheralDrivers/src/adc_0.OBJ: C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.1.6/Device/EFM8UB2/peripheral_driver/inc/adc_0.h C:/SiliconLabs/SimplicityStudio/v4/developer/toolchains/keil_8051/9.53/INC/ASSERT.H C:/Users/apeters.SPECSERV/Projects/EFM8-USB-MIDI/inc/config/efm8_config.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.1.6/Device/EFM8UB2/inc/SI_EFM8UB2_Register_Enums.h C:/SiliconLabs/SimplicityStudio/v4/developer/toolchains/keil_8051/9.53/INC/STDIO.H C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.1.6/Device/EFM8UB2/inc/SI_EFM8UB2_Defs.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.1.6/Device/shared/si8051Base/si_toolchain.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.1.6/Device/shared/si8051Base/stdint.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.1.6/Device/shared/si8051Base/stdbool.h
+lib/efm8ub2/peripheralDrivers/src/adc_0.OBJ: /Users/andy/Projects/Workspaces/SiLabs_8051/EFM8-USB-MIDI/lib/efm8ub2/peripheralDrivers/inc/adc_0.h /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/keil_8051/9.53/INC/ASSERT.H /Users/andy/Projects/Workspaces/SiLabs_8051/EFM8-USB-MIDI/inc/config/efm8_config.h /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/8051/v4.1.6/Device/EFM8UB2/inc/SI_EFM8UB2_Register_Enums.h /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/keil_8051/9.53/INC/STDIO.H /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/8051/v4.1.6/Device/EFM8UB2/inc/SI_EFM8UB2_Defs.h /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/8051/v4.1.6/Device/shared/si8051Base/si_toolchain.h /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/8051/v4.1.6/Device/shared/si8051Base/stdint.h /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/8051/v4.1.6/Device/shared/si8051Base/stdbool.h
 
-lib/efm8ub2/peripheralDrivers/src/spi_0.OBJ: C:/Users/apeters.SPECSERV/Projects/EFM8-USB-MIDI/inc/config/efm8_config.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.1.6/Device/EFM8UB2/inc/SI_EFM8UB2_Register_Enums.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.1.6/Device/EFM8UB2/peripheral_driver/inc/spi_0.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.1.6/Device/EFM8UB2/inc/SI_EFM8UB2_Defs.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.1.6/Device/shared/si8051Base/si_toolchain.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.1.6/Device/shared/si8051Base/stdint.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.1.6/Device/shared/si8051Base/stdbool.h
+lib/efm8ub2/peripheralDrivers/src/spi_0.OBJ: /Users/andy/Projects/Workspaces/SiLabs_8051/EFM8-USB-MIDI/inc/config/efm8_config.h /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/8051/v4.1.6/Device/EFM8UB2/inc/SI_EFM8UB2_Register_Enums.h /Users/andy/Projects/Workspaces/SiLabs_8051/EFM8-USB-MIDI/lib/efm8ub2/peripheralDrivers/inc/spi_0.h /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/8051/v4.1.6/Device/EFM8UB2/inc/SI_EFM8UB2_Defs.h /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/8051/v4.1.6/Device/shared/si8051Base/si_toolchain.h /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/8051/v4.1.6/Device/shared/si8051Base/stdint.h /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/8051/v4.1.6/Device/shared/si8051Base/stdbool.h
 
-lib/efm8ub2/peripheralDrivers/src/usb_0.OBJ: C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.1.6/Device/EFM8UB2/peripheral_driver/inc/usb_0.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.1.6/Device/shared/si8051Base/stdbool.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.1.6/Device/shared/si8051Base/stdint.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.1.6/Device/EFM8UB2/inc/SI_EFM8UB2_Register_Enums.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.1.6/Device/EFM8UB2/inc/SI_EFM8UB2_Defs.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.1.6/Device/shared/si8051Base/si_toolchain.h
+lib/efm8ub2/peripheralDrivers/src/uart_1.OBJ: /Users/andy/Projects/Workspaces/SiLabs_8051/EFM8-USB-MIDI/lib/efm8ub2/peripheralDrivers/inc/uart_1.h /Users/andy/Projects/Workspaces/SiLabs_8051/EFM8-USB-MIDI/inc/config/efm8_config.h /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/8051/v4.1.6/Device/EFM8UB2/inc/SI_EFM8UB2_Register_Enums.h /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/8051/v4.1.6/Device/EFM8UB2/inc/SI_EFM8UB2_Defs.h /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/8051/v4.1.6/Device/shared/si8051Base/si_toolchain.h /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/8051/v4.1.6/Device/shared/si8051Base/stdint.h /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/8051/v4.1.6/Device/shared/si8051Base/stdbool.h
+
+lib/efm8ub2/peripheralDrivers/src/usb_0.OBJ: /Users/andy/Projects/Workspaces/SiLabs_8051/EFM8-USB-MIDI/lib/efm8ub2/peripheralDrivers/inc/usb_0.h /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/8051/v4.1.6/Device/shared/si8051Base/stdbool.h /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/8051/v4.1.6/Device/shared/si8051Base/stdint.h /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/8051/v4.1.6/Device/EFM8UB2/inc/SI_EFM8UB2_Register_Enums.h /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/8051/v4.1.6/Device/EFM8UB2/inc/SI_EFM8UB2_Defs.h /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/8051/v4.1.6/Device/shared/si8051Base/si_toolchain.h
 
 
