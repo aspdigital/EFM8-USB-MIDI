8b3cbb5eb25cd26e4b738a1b5542b194858111c8
andy <andy@ae0baf16-264e-11e7-85bd-8384b197c6a9>
2019-05-14 23:58:12 +0000

    Device state change (to configure) interrupts and invokes a callback. Do the first USBD_Read() in that callback.

--- src/callback.c
+++ src/callback.c
@@ -33,8 +33,28 @@ extern SI_SEGMENT_VARIABLE(EndpointBuffer[SLAB_USB_EP1OUT_MAX_PACKET_SIZE], uint
 // Functions
 //-----------------------------------------------------------------------------
 
+#if SLAB_USB_SOF_CB
 void USBD_SofCb(uint16_t sofNr) {
 	UNREFERENCED_ARGUMENT(sofNr);
+}
+#endif
+
+/*
+ * When state changes to connected, arm the endpoint and enable the callback.
+ * I don't think there's a way to cancel this.
+ */
+void USBD_DeviceStateChangeCb(USBD_State_TypeDef oldState,
+                              USBD_State_TypeDef newState) {
+	if (newState == USBD_STATE_CONFIGURED) {
+#if USE_SLAB_EP1OUT_HANDLER == 0
+		USBD_Read(EP1OUT, (uint8_t *) &EndpointBuffer, sizeof(MIDI_Event_Packet_t), // midi messages are four bytes
+				false); // callback not implemented.
+#else
+		USBD_Read(EP1OUT, &EndpointBuffer, SLAB_USB_EP1OUT_MAX_PACKET_SIZE, // midi messages are four bytes
+		true); // use callback to move data from endpoint to MIDI FIFO.
+#endif
+
+	}
 }
 
 /*
@@ -58,6 +78,8 @@ uint16_t USBD_XferCompleteCb(uint8_t epAddr, USB_Status_TypeDef status,
 		MIDIFIFO_Push(&mep);
 		xferred -= 4;
 	}
+	USBD_Read(EP1OUT, &EndpointBuffer, SLAB_USB_EP1OUT_MAX_PACKET_SIZE, // midi messages are four bytes
+			true); // use callback to move data from endpoint to MIDI FIFO.
 #else
 	UNREFERENCED_ARGUMENT(xferred);
 #endif
